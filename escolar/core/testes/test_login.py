f93ed197f4051f7eb4d639e40ae4cf61b37ae29c
Fabiano Almeida <fabiano@znc.com.br>
2016-11-14 18:49:41 -0200

    ref #2 testes ref login

--- escolar/core/testes/test_login.py
+++ escolar/core/testes/test_login.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url
+from escolar.core.forms import AuthenticationForm
 
 class LoginTest(TestCase):
 
@@ -7,7 +8,7 @@ class LoginTest(TestCase):
         self.response = self.client.get(resolve_url('login'))
 
     def test_get(self):
-        """GET / must return status code 200"""
+        """GET /login/ must return status code 200"""
         # response = self.client.get('/')
         self.assertEqual(200, self.response.status_code)
 
@@ -16,11 +17,29 @@ class LoginTest(TestCase):
         # response = self.client.get('/')
         self.assertTemplateUsed(self.response, 'login.html')
 
+    def test_html(self):
+        """Must conatain input tags"""
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, '<input', 4)
+        self.assertContains(self.response, 'type="text"', 2)
+        self.assertContains(self.response, 'type="submit"', 1)
 
-    # def test_speackers_link(self):
-    #     """Must show keynote speakers  link"""
-    #     expected = 'href="{}#speakers"'.format(resolve_url('home'))
-    #     self.assertContains(self.response, expected)
+    def test_crsf(self):
+        """Html must coantain csrf"""
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+
+    def test_has_form(self):
+        """Context must have LoginTest form"""
+        form = self.response.context['form']
+        self.assertIsInstance(form, AuthenticationForm)
+
+    def test_form_has_fields(self):
+        """Form must have 3 fields"""
+        form = self.response.context['form']
+        self.assertSequenceEqual(['username', 'password', 'keep_me_logged_in'], list(form.fields))
+
+
 
     # def test_talks_link(self):
     #     """Must show keynote talks link"""
