9d7b303aff87d59a53f35a776a46c19f2182c5d1
Fabiano Almeida <fabiano@znc.com.br>
2016-11-13 23:27:20 -0200

    ref #3 404, 500 e login; test para home 200 e template usado

---
+++ escolar/core/testes/test_view_home.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+# from django.shortcuts import resolve_url
+
+class HomeTest(TestCase):
+    # fixtures = ['keynotes.json']
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        # response = self.client.get('/')
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use index.html"""
+        # response = self.client.get('/')
+        self.assertTemplateUsed(self.response, 'index.html')
+
+
+    # def test_speackers_link(self):
+    #     """Must show keynote speakers  link"""
+    #     expected = 'href="{}#speakers"'.format(resolve_url('home'))
+    #     self.assertContains(self.response, expected)
+
+    # def test_talks_link(self):
+    #     """Must show keynote talks link"""
+    #     expected = 'href="{}"'.format(resolve_url('talk_list'))
+    #     self.assertContains(self.response, expected)
