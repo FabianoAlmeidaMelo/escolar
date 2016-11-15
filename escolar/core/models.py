bc5191acbe3a4039b4fa9d7e12ddf62fae9f1d2a
Fabiano Almeida <fabiano@znc.com.br>
2016-11-15 17:10:07 -0200

    ref #4 User, testes para o model, sem migração, usando o django-test-without-migrations

--- escolar/core/models.py
+++ escolar/core/models.py
@@ -1,3 +1,69 @@
-from django.db import models
+# coding: utf-8
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import(
+    AbstractBaseUser,
+    BaseUserManager,
+)
+
+SEXO = (
+    (1, "M"),
+    (2, "F"),
+    )
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        now = timezone.now()
+        if not email:
+            raise ValueError(u'Email é obrigatório')
+        email = UserManager.normalize_email(email)
+        user = self.model(email=email, is_active=True, is_superuser=False,
+                          last_login=now, date_joined=now, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_active = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    '''
+        Usuário do sistema
+        is_superuser == usuário Administrador
+    '''
+    email = models.EmailField('e-mail', unique=True)
+    nome = models.CharField(verbose_name=u'Nome', max_length=100)
+    is_active = models.BooleanField('ativo', default=True,)
+    created_at = models.DateTimeField('data de cadastro', auto_now_add=True)
+    nascimento = models.DateField(u'Data Nascimento', null=True, blank=True)
+    profissao = models.CharField(
+        u'Profissão', max_length=100, null=True, blank=True
+        )
+    sexo = models.IntegerField(u'Sexo', choices=SEXO, null=True, blank=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('nome', )
+
+    class Meta:
+        verbose_name = u'Usuário'
+        verbose_name_plural = u'Usuários'
+
+    def __unicode__(self):
+        return self.email
+
+# class Escola(models.Model):
+#     nome=models.CharField(max_length=200)
+#     endereco=models.CharField(max_length=200)
+#     numero=models.CharField(max_length=10)
+#     telefone=models.CharField(max_length=12)
+
---
+++ escolar/core/testes/test_models_user.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from django.test import TestCase
+
+from escolar.core.models import User
+
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.obj = User(
+                        email='email@usuario.com',
+                        nome='usuario nome',
+                        is_active=True,
+                        nascimento='2005-09-20',
+                        profissao='Aluno',
+                        sexo=2,
+                        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_created_at(self):
+        """User must have an auto created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
