20e31383421f31a426c1566f88dd308aed3503e8
Fabiano Almeida <fabiano@znc.com.br>
2016-11-28 23:12:42 -0200

    ref check to django 1.10 : url 'Pass the callable instead'

--- escolar/urls.py
+++ escolar/urls.py
@@ -1,7 +1,14 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth.views import logout, login
+from django.contrib.auth.views import (
+            logout,
+            login,
+            password_reset,
+            password_reset_complete,
+            password_reset_confirm,
+            password_reset_done,
+        )
 from escolar.core.forms import AuthenticationForm
 
 from escolar.core.views import home
@@ -10,33 +17,20 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     # Logins
     url(r'^logout/$', logout, {"next_page": "/"}, name="logout"),
-    url(r'^login/$',
-        login,
-        {
-         'template_name': 'login.html',
-         'authentication_form': AuthenticationForm,
-        },
-        name="login"),
+    url(r'^login/$', login, {"template_name": 'login.html', "authentication_form": AuthenticationForm,}, name="login"),
 
     # password Reset
     url(
         r'^user/password/reset/$',
-        'django.contrib.auth.views.password_reset',
+        password_reset,
         {'post_reset_redirect': '/user/password/reset/done/'},
         name="password_reset"
     ),
-    url(
-        r'^user/password/reset/done/$',
-        'django.contrib.auth.views.password_reset_done'
-    ),
+    url(r'^user/password/reset/done/$', password_reset_done),
     url(
         r'^user/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
-        'django.contrib.auth.views.password_reset_confirm',
-        {'post_reset_redirect': '/user/password/done/'}
+        password_reset_confirm,{'post_reset_redirect': '/user/password/done/'}
     ),
-    url(
-        r'^user/password/done/$',
-        'django.contrib.auth.views.password_reset_complete'
-    ),
+    url(r'^user/password/done/$', password_reset_complete),
     url(r'^admin/', admin.site.urls),
 ]
