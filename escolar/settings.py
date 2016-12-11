7376e60b5d479fb2b0e41ac2e004226421a1bbfa
Fabiano Almeida <fabiano@znc.com.br>
2016-12-11 13:12:07 -0200

    ref #8 remove os arquivos ref Materialize e ajusta os static

--- escolar/settings.py
+++ escolar/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'test_without_migrations',
+    'bootstrap_toolkit',
     'municipios',
     'escolar.core',
     'escolar.escolas.apps.EscolasConfig',
