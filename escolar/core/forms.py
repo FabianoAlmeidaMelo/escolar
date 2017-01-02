cfa4cb1312c018b24b33671bf5487095e53029f6
Fabiano Almeida <fabiano@znc.com.br>
2017-01-02 13:52:23 -0200

    ref #13 ajustes no form do User

--- escolar/core/forms.py
+++ escolar/core/forms.py
@@ -42,7 +42,7 @@ class UserForm(forms.ModelForm):
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
+        password2 = self.cleaned_data.get("password2") 
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords diferentes")
         elif password1 and not password2:
