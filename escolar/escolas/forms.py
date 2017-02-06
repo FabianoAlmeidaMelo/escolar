1eec9960ad561f1b5640bf22bb6af7a41b8ad07d
Fabiano Almeida <fabiano@znc.com.br>
2017-02-05 22:40:25 -0200

    ref #20 exclui do queryset, alunos que já estão distribuidos em uma sala do mesmo ano e eescola

--- escolar/escolas/forms.py
+++ escolar/escolas/forms.py
@@ -57,9 +57,13 @@ class ClasseAlunoForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.classe = kwargs.pop('classe', None)
         super(ClasseAlunoForm, self).__init__(*args, **kwargs)
+
         escola = Escola.objects.get(id=self.classe.escola.id)
         alunos_ids = UserGrupos.objects.filter(escola=escola, grupo__name='Aluno').values_list('user__id', flat=True)
-        self.fields['aluno'].queryset = User.objects.filter(id__in=alunos_ids)
+        classes_concorrentes_ids = Classe.objects.filter(ano=2017).values_list('id', flat=True)
+        alunos_distribuidos_ids =ClasseAluno.objects.filter(id__in=classes_concorrentes_ids).values_list('aluno__id', flat=True)
+
+        self.fields['aluno'].queryset = User.objects.filter(id__in=alunos_ids).exclude(id__in=alunos_distribuidos_ids)
 
     def save(self, *args, **kwargs):
         self.instance.classe = self.classe
