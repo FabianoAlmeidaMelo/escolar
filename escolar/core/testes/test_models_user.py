86c9d0338230980263e0152c9099f7a4f4402d00
Fabiano Almeida <fabiano@znc.com.br>
2018-03-25 22:47:46 -0300

    ref #63 test model user

--- escolar/core/testes/test_models_user.py
+++ escolar/core/testes/test_models_user.py
@@ -3,15 +3,17 @@ from django.test import TestCase
 
 from escolar.core.models import User
 
+'''
+python manage.py test escolar.core.testes.test_models_user
+'''
+
 class UserModelTest(TestCase):
     def setUp(self):
         self.obj = User(
                         email='email@usuario.com',
+                        username='email@usuario.com',
                         nome='usuario nome',
                         is_active=True,
-                        nascimento='2005-09-20',
-                        profissao='Aluno',
-                        sexo=2,
                         )
         self.obj.save()
 
@@ -20,7 +22,7 @@ class UserModelTest(TestCase):
 
     def test_created_at(self):
         """User must have an auto created_at attr"""
-        self.assertIsInstance(self.obj.created_at, datetime)
+        self.assertIsInstance(self.obj.date_joined, datetime)
 
-    def test_unicode(self):
-        self.assertEqual(str(self.obj), 'email@usuario.com')
+    def test_str(self):
+        self.assertEqual(str(self.obj), 'usuario nome')
