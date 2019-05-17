6f9e937545b82fa71e43cbe0e8826fea41fe2fe7
Fabiano Almeida <fabiano@znc.com.br>
2019-05-16 23:27:12 -0300

    altera .sh

--- escolar/scripts/alerta_aniversariantes_do_dia.sh
+++ escolar/scripts/alerta_aniversariantes_do_dia.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ubuntu/.virtualenvs/escolar/bin/activate
-PYTHONIOENCODING=utf_8 /var/www/projetos/escolar/escolar/manage.py alerta_aniversariantes_do_dia $1
+PYTHONIOENCODING=utf_8 python /var/www/projetos/escolar/manage.py alerta_aniversariantes_do_dia $1
