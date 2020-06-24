3307d69019c74a87a71c3b5629d3946dec84f57d
Fabiano Almeida <fabianoalmeidasjc@gmail.com>
2020-06-24 15:08:15 -0300

    ref #2 versão final da view_inadimplentes

--- escolar/sql_utils/views/view_inadimplentes.sql
+++ escolar/sql_utils/views/view_inadimplentes.sql
@@ -3,12 +3,12 @@
 -- DROP VIEW public.view_inadimplentes;
 
 CREATE OR REPLACE VIEW view_inadimplentes AS 
- SELECT
-    row_number() OVER () AS id,
+ SELECT row_number() OVER () AS id,
+    contrato.ano,
     pagamento.escola_id,
     pagamento.contrato_id,
     count(pagamento.id) AS pagamentos_atrasados,
-    array_agg(pagamento.titulo) as titulos,
+    array_agg(pagamento.titulo) AS titulos,
     sum(pagamento.valor) AS valor,
     sum(
         CASE
@@ -20,11 +20,11 @@ CREATE OR REPLACE VIEW view_inadimplentes AS
             WHEN pagamento.categoria_id = 1 THEN contratoaluno.juros / 30::numeric * (now()::date - pagamento.data)::numeric / 100::numeric * pagamento.valor
             ELSE 0::numeric
         END) AS juros,
-    pessoa.cpf as cpf_resp_fin,
-    pessoa.nome as responsavel_nome,
+    pessoa.cpf AS cpf_resp_fin,
+    pessoa.nome AS responsavel_nome,
     pessoa.celular,
     pessoa.email,
-    aluno.nome as nome_aluno,
+    aluno.nome AS aluno_nome,
     contratoaluno.serie_id
    FROM financeiro_pagamento pagamento
      LEFT JOIN financeiro_contrato contrato ON contrato.id = pagamento.contrato_id
@@ -32,7 +32,5 @@ CREATE OR REPLACE VIEW view_inadimplentes AS
      LEFT JOIN escolas_membrofamilia membro_familia ON membro_familia.pessoa_ptr_id = contratoaluno.responsavel_id
      LEFT JOIN escolas_pessoa pessoa ON pessoa.id = contratoaluno.responsavel_id
      LEFT JOIN escolas_pessoa aluno ON aluno.id = contratoaluno.aluno_id
-  WHERE pagamento.efet = false 
-  AND pagamento.data < now()::date
-  AND contrato.id is NOT NULL
-  GROUP BY pagamento.escola_id, pagamento.contrato_id, pessoa.cpf, pessoa.nome, pessoa.celular, pessoa.email, aluno.nome, contratoaluno.serie_id;
+  WHERE pagamento.efet = false AND pagamento.data < now()::date AND contrato.id IS NOT NULL
+  GROUP BY pagamento.escola_id, pagamento.contrato_id, pessoa.cpf, pessoa.nome, pessoa.celular, pessoa.email, aluno.nome, contratoaluno.serie_id, contrato.ano;
